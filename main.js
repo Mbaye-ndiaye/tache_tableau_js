0e6a1f92e2e18a3dc00a5c5cfd91dd4c7810de0a
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-08-15 23:20:14 +0000

    tache tableau

---
+++ main.js
@@ -0,0 +1,23 @@
+// creer un tableau
+var mois = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet']
+
+// retirer le dernier valeur du tableau moi
+mois.pop();
+
+// afficher le resultat
+// document.write(mois);
+
+// ajouter la valeur 'aout' a la fin du tableau
+mois.push('aoùt');
+
+// document.write(mois)
+
+/* remplacer la valeur 'fevrier' par 'fevrier'
+supprimer une valeur && et ajouter une valeur a sa palce
+*/
+mois.splice(1,1, 'fevrier')
+// document.write(mois)
+
+// afficher la troisieme valeur du tableau
+
+document.write('avril');
